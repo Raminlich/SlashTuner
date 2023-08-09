6d99569cd668c0dd7518002c1006ccd11731aafd
RaminDashti <ramin.dashti@hotmail.com>
2023-08-10 00:18:33 +0330

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     private float turnSmoothVelocity;
     private float speed;
     private Vector3 movementDirection;
+    private Vector2 moveInputValue;
 
     private void OnEnable()
     {
@@ -31,39 +32,6 @@ public class PlayerController : MonoBehaviour
         sprintInput.performed += OnSprintTrigger;
         sprintInput.canceled += OnSprintCanceled;
         playerInputs.Enable();
-
-    }
-
-    private void OnSprintCanceled(InputAction.CallbackContext context)
-    {
-        SetPlayerSate(CharacterState.Walk);
-        speed = walkSpeed;
-    }
-
-    private void OnSprintTrigger(InputAction.CallbackContext context)
-    {
-        speed = walkSpeed + sprintSpeed;
-        SetPlayerSate(CharacterState.Sprint);
-
-    }
-
-    private void OnAttackTrigger(InputAction.CallbackContext context)
-    {
-        //animator.SetTrigger("AttackTrigger");
-        if(characterState != CharacterState.Roll) 
-        {
-            SetPlayerSate(CharacterState.Attack);
-            animator.SetBool("OnAttack", true);
-        }
-
-    }
-
-    private void OnRollTrigger(InputAction.CallbackContext context)
-    {
-        animator.SetTrigger("RollTrigger");
-        SetPlayerSate(CharacterState.Roll);
-        animator.SetFloat("CharacterSpeed", 0);
-
     }
 
     private void OnDisable()
@@ -78,15 +46,51 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
 
     }
+
     void Start()
     {
         speed = walkSpeed;
     }
+
     void Update()
     {
-        Movement();
-        SetGravity();
+        GetInput();
+        ApplyMovement();
+    }
 
+    private void GetInput()
+    {
+        moveInputValue = moveInput.ReadValue<Vector2>();
+        if (moveInputValue != Vector2.zero)
+            StartMove();
+        else
+            CancelMove();
+    }
+
+    private void StartMove()
+    {
+        var moveDirection = transform.TransformDirection(new Vector3(moveInputValue.x, 0, moveInputValue.y));
+        if (characterState == CharacterState.Roll || characterState == CharacterState.Attack) return;
+        animator.SetBool("IsWalking", true);
+        movementDirection = moveDirection * speed * Time.deltaTime;
+        ApplyCharacterRotation();
+    }
+
+    private void CancelMove()
+    {
+        SetPlayerSate(CharacterState.Idle);
+        animator.SetBool("IsWalking", false);
+        movementDirection = Vector3.zero;
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection += GetGravityMovementValue();
+        characterController.Move(movementDirection);
+
+        animator.SetFloat("CharacterSpeed", (float)(Mathf.Clamp(characterController.velocity.sqrMagnitude, 0f, 12.5f) / 12.5));
+        animator.SetFloat("TrajecetoryForwad", moveInputValue.x);
+        animator.SetFloat("TrajecetorySide", moveInputValue.y);
     }
 
     public void SetPlayerSate(CharacterState state)
@@ -94,57 +98,48 @@ public class PlayerController : MonoBehaviour
         characterState = state;
     }
 
-    private void Movement()
-    {
-        if (characterState != CharacterState.Attack && characterState != CharacterState.Roll)
-        {
-            Vector2 inputValue = moveInput.ReadValue<Vector2>();
-            var xMotion = inputValue.y;
-            var zMotion = inputValue.x;
-            if (inputValue != Vector2.zero)
-            {
-                animator.SetBool("IsWalking", true);
-                Vector3 moveDirection = transform.TransformDirection(new Vector3(inputValue.x, 0, inputValue.y));
-                movementDirection = moveDirection * speed * Time.deltaTime;
-                characterController.Move(movementDirection);
-                RotateTowardDirection();
-
-            }
-            else
-            {
-                SetPlayerSate(CharacterState.Idle);
-                animator.SetBool("IsWalking", false);
-            }
-            animator.SetFloat("CharacterSpeed", (float)(Mathf.Clamp(characterController.velocity.sqrMagnitude,0f,12.5f)/12.5));
-            animator.SetFloat("TrajecetoryForwad",zMotion);
-            animator.SetFloat("TrajecetorySide",xMotion);
-        }
-
-    }
-
-    private void SetGravity()
-    {
-        if(!characterController.isGrounded)
-        {
-            Debug.Log("Is not grounded");
-            characterController.Move(new Vector3(0, gravity, 0) * Time.deltaTime * gravityMultiplier);
-        }
-        else
-        {
-            Debug.Log("Is grounded");
-
-        }
-    }
-
-    private void RotateTowardDirection()
+    private void ApplyCharacterRotation()
     {
         var direction = Camera.main.transform.eulerAngles;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, direction.y, ref turnSmoothVelocity, turnSmooth);
         transform.rotation = Quaternion.Euler(0, angle, 0);
     }
+
+    private Vector3 GetGravityMovementValue()
+    {
+        return new Vector3(0, gravity, 0) * Time.deltaTime * gravityMultiplier;
+    }
+
+    private void OnSprintTrigger(InputAction.CallbackContext context)
+    {
+        speed = walkSpeed + sprintSpeed;
+        SetPlayerSate(CharacterState.Sprint);
+    }
+
+    private void OnSprintCanceled(InputAction.CallbackContext context)
+    {
+        SetPlayerSate(CharacterState.Walk);
+        speed = walkSpeed;
+    }
+
+    private void OnAttackTrigger(InputAction.CallbackContext context)
+    {
+        if (characterState != CharacterState.Roll)
+        {
+            SetPlayerSate(CharacterState.Attack);
+            animator.SetBool("OnAttack", true);
+        }
+        movementDirection = Vector3.zero;
+    }
+
+    private void OnRollTrigger(InputAction.CallbackContext context)
+    {
+        animator.SetTrigger("RollTrigger");
+        SetPlayerSate(CharacterState.Roll);
+        animator.SetFloat("CharacterSpeed", 0);
+        movementDirection = Vector3.zero;
+    }
 }
-
-
 public enum CharacterState
 {
     Idle,
